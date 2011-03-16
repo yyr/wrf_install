3934e0d9b1899a912445547a573594ed347b953a
Rick Morgans <rick.morgans@cyclopicenergy.com>
2011-03-16 22:05:43 +1030

    major changes

--- download_wrf.sh
+++ download_wrf.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #Settings
-INSTALL_PATH=/opt/wrf
+. SETTINGS.env
+
+# Setup source directory
 mkdir -p $INSTALL_PATH/src
-cd $INSTALL_PATH/src
-ENV=$INSTALL_PATH/src/wrf.env
-echo 'export INSTALL_PATH='$INSTALL_PATH > $ENV
 CLEAN_DOWN=$INSTALL_PATH/src/wrf_clean_downloads.sh
 echo '#!/bin/bash' > $CLEAN_DOWN
 chmod +x $CLEAN_DOWN
@@ -12,77 +11,11 @@ CLEAN_SRC=$INSTALL_PATH/src/wrf_clean_source.sh
 echo '#!/bin/bash' > $CLEAN_SRC
 chmod +x $CLEAN_SRC
 
-#JASPER
-APP=jasper-1.900.1
-EXT=zip
-echo 'export JASPER='$APP >> $ENV
-echo rm ${APP}.${EXT} >> $CLEAN_DOWN
-cd $INSTALL_PATH/src
-wget -c http://www.ece.uvic.ca/~mdadams/jasper/software/${APP}.${EXT}
-unzip ${APP}.${EXT}
-echo rm -rf ${APP} >> $CLEAN_SRC
-
-#JPEG
-APP=jpegsrc.v8c
-EXT=tar.gz
-echo 'export JPEG='$APP >> $ENV
-chmod +x $ENV
-echo rm ${APP}.${EXT} >> $CLEAN_DOWN
-cd $INSTALL_PATH/src
-wget -c http://www.ijg.org/files/${APP}.${EXT}
-tar zxf ${APP}.${EXT}
-echo rm -rf jpeg-8c >> $CLEAN_SRC
-
-# ZLIB
-APP=zlib-1.2.5
-EXT=tar.gz
-echo 'export ZLIB='$APP >> $ENV
-echo rm ${APP}.${EXT} >> $CLEAN_DOWN
-cd $INSTALL_PATH/src
-wget -c http://zlib.net/${APP}.tar.gz
-tar zxf ${APP}.${EXT}
-echo rm -rf ${APP} >> $CLEAN_SRC
-
-# HDF5
-APP=hdf5-1.8.6
-EXT=tar.gz
-echo 'export HDF5='$APP >> $ENV
-echo rm ${APP}.${EXT} >> $CLEAN_DOWN
-cd $INSTALL_PATH/src
-wget -c http://www.hdfgroup.org/ftp/HDF5/${APP}/src/${APP}.${EXT}
-tar xzf ${APP}.${EXT}
-echo rm -rf ${APP} >> $CLEAN_SRC
-
-# NetCDF4
-APP=netcdf-4.1.1
-EXT=tar.gz
-echo 'export NETCDF4='$APP >> $ENV
-echo rm ${APP}.${EXT} >> $CLEAN_DOWN
-cd $INSTALL_PATH/src
-wget -c ftp://ftp.unidata.ucar.edu/pub/netcdf/${APP}.${EXT}
-tar xzf ${APP}.${EXT}
-echo rm -rf ${APP} >> $CLEAN_SRC
-
-# WRF
-APP=WRFV3.2.1
-EXT=TAR.gz
-WRF_PATH=$INSTALL_PATH/$APP
-echo 'export WRF='$APP >> $ENV
-echo rm ${APP}.${EXT} >> $CLEAN_DOWN
-cd $INSTALL_PATH/src
-wget -c http://www.mmm.ucar.edu/wrf/src/${APP}.${EXT}
-tar xzf ${APP}.${EXT}
-echo rm -rf WRFV3 >> $CLEAN_SRC
-
-# WPS
-APP=WPSV3.2.1
-EXT=TAR.gz
-WPS_PATH=$INSTALL_PATH/$APP
-echo 'export WPS='$APP >> $ENV
-echo rm ${APP}.${EXT} >> $CLEAN_DOWN
-cd $INSTALL_PATH/src
-wget -c http://www.mmm.ucar.edu/wrf/src/${APP}.${EXT}
-tar xzf ${APP}.${EXT}
-echo rm -rf WPS >> $CLEAN_SRC
-
-echo export INTEL_COMPILER_TOPDIR=/opt/intel/Compiler/11.1/072/ >> $ENV
+for package in ${packages}
+do
+    echo Downloading... ${package}
+    . $INSTALL_PATH/${package}.env
+    echo rm ${APP}.${EXT} >> $CLEAN_DOWN
+    wget -c ${URL} -P $INSTALL_PATH/src
+    echo rm -rf ${DIR} >> $CLEAN_SRC
+done
