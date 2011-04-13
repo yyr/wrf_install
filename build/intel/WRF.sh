61eed5f96d0439ea7b139b4d91bfbef4b55f8340
Rick Morgans <rick.morgans@cyclopicenergy.com>
2011-04-13 22:56:51 +0930

    major changes - $WRF_BASE source $WRF_BASE/WPS.env to set up full environment

--- build/intel/WRF.sh
+++ build/intel/WRF.sh
@@ -1,17 +1,6 @@
 ##!/bin/bash
-COMP=intel
-. $INSTALL_PATH/JASPER.env
-JASPER_PATH=$INSTALL_PATH/bin/$COMP/$APP
-. $INSTALL_PATH/ZLIB.env
-ZLIB_PATH=$INSTALL_PATH/bin/$COMP/$APP
-. $INSTALL_PATH/SZIP.env
-SZIP_PATH=$INSTALL_PATH/bin/$COMP/$APP
-. $INSTALL_PATH/HDF5.env
-HDF5_PATH=$INSTALL_PATH/bin/$COMP/$APP
-. $INSTALL_PATH/NETCDF4.env
-NETCDF4_PATH=$INSTALL_PATH/bin/$COMP/$APP
-. $INSTALL_PATH/WRF.env
-cd $INSTALL_PATH/src/
+. $WRF_BASE/WRF.env
+cd $WRF_ROOT
 rm -r ${DIR}
 tar zxf ${APP}.${EXT}
 wget http://www.mmm.ucar.edu/wrf/src/fix/configure_fix.tar
