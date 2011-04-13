e7cd09c63a6c064226b087581578a54b11762b5f
Rick Morgans <rick.morgans@cyclopicenergy.com>
2011-04-14 00:23:21 +0930

    mod $WRF_BASE added OPENMPI.env

--- build/intel/ZLIB.sh
+++ build/intel/ZLIB.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-COMP=intel
-. $INSTALL_PATH/ZLIB.env
-cd $INSTALL_PATH/src/
+. $WRF_ROOT/ZLIB.env
+cd $WRF_ROOT/src/
 rm -r ${DIR}
 tar zxf ${APP}.${EXT}
 cd ${DIR}
-. $INTEL_COMPILER_TOPDIR/bin/intel64/iccvars_intel64.sh
-. $INTEL_COMPILER_TOPDIR/bin/intel64/ifortvars_intel64.sh
-#export CC=gcc
-#export FC=gfortran
-#export CXX=''
-export CC=icc
-export CXX=icpc
-export F77=ifort
-export CFLAGS='-O3 -xP -ip'
-export CXXFLAGS='-O3 -xP -ip'
-export FFLAGS='-O3 -xP -ip'
 rm CMakeCache.txt
 cmake -DCMAKE_INSTALL_PREFIX:path=/opt/wrf/bin/intel/zlib-1.2.5 . | tee ${APP}.${COMP}.config
 make clean 2>&1 | tee ${APP}.${COMP}.clean
