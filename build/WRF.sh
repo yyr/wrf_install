b03b6a07c0da3febfca2c5b2e43ee1555a63aa79
Rick Morgans <rick.morgans@cyclopicenergy.com>
2011-04-20 00:14:40 +0930

    WRF

--- build/WRF.sh
+++ build/WRF.sh
@@ -1,9 +1,9 @@
 ##!/bin/bash
 . $WRF_BASE/WRF.env
+cd $WRF_BASE/src
+tar zxf cache/${APP}.${EXT}
+mv ${DIR} $WRF_ROOT
 cd $WRF_ROOT
-rm -r ${DIR}
-tar zxf ${APP}.${EXT}
 wget http://www.mmm.ucar.edu/wrf/src/fix/configure_fix.tar
 tar xf configure_fix.tar
 chmod +x configure
-cd ${DIR}
