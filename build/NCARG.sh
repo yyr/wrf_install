b883e934b80d22ebfe084ad402d3dcd06fe66e5e
Rick Morgans <rick.morgans@cyclopicenergy.com>
2011-04-20 01:07:32 +0930

    WPS and NCARG

---
+++ build/NCARG.sh
@@ -0,0 +1,4 @@
+##!/bin/bash
+. $WRF_BASE/NCARG.env
+cd $NCARG_ROOT
+tar zxf $WRF_BASE/src/cache/ncl_ncarg-6.0.0-beta.Linux_Debian_x86_64_nodap_gcc445.tar.gz
--- build/WPS.sh
+++ build/WPS.sh
@@ -2,5 +2,5 @@
 . $WRF_BASE/WPS.env
 cd $WRF_BASE/src
 tar zxf cache/${APP}.${EXT}
-mv ${DIR} $WRF_ROOT
-cd $WRF_ROOT
+mv ${DIR} $WPS_ROOT
+cd $WPS_ROOT
