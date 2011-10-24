42f9617314496987802ea27a53b6ab17d6f886dc
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-10-25 06:26:14 +0900

    * download_wrf.sh: extraction file also created

--- download_wrf.sh
+++ download_wrf.sh
@@ -2,6 +2,7 @@
 packages="HDF5 JASPER NETCDF4 SZIP ZLIB JPEG WPS WRF GRADS"
 
 INSTALL_PATH=~/wrf/
+WRF_BASE=$INSTALL_PATH
 # Setup source directory
 mkdir -p $INSTALL_PATH/src      # make
 
@@ -11,9 +12,14 @@ cp *.env $INSTALL_PATH/
 CLEAN_DOWN=$INSTALL_PATH/src/wrf_clean_downloads.sh
 echo '#!/bin/bash' > $CLEAN_DOWN
 chmod +x $CLEAN_DOWN
+
 CLEAN_SRC=$INSTALL_PATH/src/wrf_clean_source.sh
 echo '#!/bin/bash' > $CLEAN_SRC
 chmod +x $CLEAN_SRC
+
+EXTRACT_SRC=$INSTALL_PATH/src/wrf_extract_source.sh
+echo '#!/bin/bash' > $EXTRACT_SRC
+chmod +x $EXTRACT_SRC
 
 for package in ${packages}
 do
@@ -22,4 +28,14 @@ do
     echo rm ${APP}.${EXT} >> $CLEAN_DOWN
     wget -c ${URL} -P $INSTALL_PATH/src
     echo rm -rf ${DIR} >> $CLEAN_SRC
+
+    case ${EXT} in
+        *".gz" )
+            echo tar xvf ${APP}.${EXT} >> $EXTRACT_SRC ;;
+        *"zip" )
+            echo unzip ${APP}.${EXT} >> $EXTRACT_SRC ;;
+        * )
+            echo echo "dont know how to extract this" ${APP}.${EXT} >> $EXTRACT_SRC ;;
+    esac
+
 done
