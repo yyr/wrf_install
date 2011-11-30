7723e2a29e22baa2f25731d169a44075f5a3667b
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-11-30 21:26:20 +0900

    * build/HDF5.sh: fix hdf exec-prefix path

--- build/HDF5.sh
+++ build/HDF5.sh
@@ -5,11 +5,13 @@
 cd $WRF_BASE/src/
 cd ${DIR}
 ./configure \
---prefix=$HDF5_ROOT \
---enable-fortran \
---with-zlib=$ZLIB_ROOT \
---with-szlib=$SZIP_ROOT \
---with-pic | tee ${APP}.${COMP}.config
+    --prefix=$HDF5_ROOT \
+    --exec-prefix=$HDF5_ROOT \
+    --enable-fortran \
+    --with-zlib=$ZLIB_ROOT \
+    --with-szlib=$SZIP_ROOT \
+    --with-pic | tee ${APP}.${COMP}.config
+
 make clean 2>&1 | tee ${APP}.${COMP}.clean
 make 2>&1 | tee ${APP}.${COMP}.make
 make check 2>&1 | tee ${APP}.${COMP}.check
