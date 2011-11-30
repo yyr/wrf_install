dacc0ad896cd9f0193da4dff809bf0df8c15749a
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-12-01 02:48:51 +0900

    * download_wrf.sh: cleaned up; sane quality, with capability of   individual downloads

--- download_wrf.sh
+++ download_wrf.sh
@@ -1,41 +1,115 @@
 #!/bin/bash
-packages="HDF5 JASPER NETCDF4 SZIP ZLIB JPEG WPS WRF GRADS"
 
-INSTALL_PATH=~/wrf/
-WRF_BASE=$INSTALL_PATH
-# Setup source directory
-mkdir -p $INSTALL_PATH/src      # make
+function download_package()
+{
+    pack=$1
+    echo Downloading... ${pack}
+    echo
+    . $SCRIPTS_DIR/${pack}.env
+    wget -c ${URL} -P $WRF_BASE/src
 
-# copy all env file to install path
-cp *.env $INSTALL_PATH/
+    if [ $? -ne 0 ]; then
+        echo "FAILED TO DOWNLOAD.. $pack ;("
+            return 64
+    else
+        echo
+        echo "Finished Downloading... $pack :)"
+    fi
 
-CLEAN_DOWN=$INSTALL_PATH/src/wrf_clean_downloads.sh
-echo '#!/bin/bash' > $CLEAN_DOWN
-chmod +x $CLEAN_DOWN
+#    touch  $WRF_BASE/src/${APP}.${EXT} # for test
 
-CLEAN_SRC=$INSTALL_PATH/src/wrf_clean_source.sh
-echo '#!/bin/bash' > $CLEAN_SRC
-chmod +x $CLEAN_SRC
+    # update generated scripts
+    for x in $util_scr; do
+        clean_line $pack $x
+    done
 
-EXTRACT_SRC=$INSTALL_PATH/src/wrf_extract_source.sh
-echo '#!/bin/bash' > $EXTRACT_SRC
-chmod +x $EXTRACT_SRC
-
-for package in ${packages}
-do
-    echo Downloading... ${package}
-    . $INSTALL_PATH/${package}.env
-    echo rm ${APP}.${EXT} >> $CLEAN_DOWN
-    wget -c ${URL} -P $INSTALL_PATH/src
-    echo rm -rf ${DIR} >> $CLEAN_SRC
-
+    echo rm ${APP}.${EXT} \# $APP >> $CLEAN_DOWN
+    echo rm -rf ${DIR} \# $APP >> $CLEAN_SRC
     case ${EXT} in
         *".gz" )
-            echo tar xvf ${APP}.${EXT} >> $EXTRACT_SRC ;;
+            echo tar xvf ${APP}.${EXT} \# $APP >> $EXTRACT_SRC ;;
         *"zip" )
-            echo unzip ${APP}.${EXT} >> $EXTRACT_SRC ;;
+            echo unzip ${APP}.${EXT} \# $APP >> $EXTRACT_SRC ;;
         * )
             echo echo "dont know how to extract this" ${APP}.${EXT} >> $EXTRACT_SRC ;;
     esac
 
+}
+
+function shebang ()
+{
+    file=$1
+    if [ ! -f $file ]; then
+        echo '#!/bin/bash' > $file
+        chmod +x $file
+        echo '#' >> $file       # just an extra comment line
+    fi
+}
+
+function clean_line ()
+{
+    perl -i -ne "print unless m!$1!" $2
+}
+
+###########################################################################
+# CODE STARTS FORM HERE
+
+all_packages="HDF5 JASPER NETCDF4 SZIP
+   ZLIB JPEG WPS WRF GRADS" # env files should be present in the SCRIPTS_DIR
+
+# check needed environment variables are present or not
+env_error=24
+if [ -z $WRF_BASE ]; then
+    echo WRF_BASE variable is empty
+    echo please set that to know where to download files
+    exit $env_error
+elif [ -z $SCRIPTS_DIR ]; then
+    echo SCRIPTS_DIR variable is empty
+    echo please set that to know where the env files located
+    exit $env_error
+fi
+
+mkdir -p $WRF_BASE/src      # Setup source directory
+
+# prepare utilitly scripts
+CLEAN_DOWN=$WRF_BASE/src/wrf_clean_downloads.sh
+CLEAN_SRC=$WRF_BASE/src/wrf_clean_source.sh
+EXTRACT_SRC=$WRF_BASE/src/wrf_extract_source.sh
+
+util_scr="$CLEAN_SRC $CLEAN_DOWN $EXTRACT_SRC"
+for f in $util_scr ; do
+    shebang $f
 done
+
+#
+counter=0
+echo no of args: $#
+while [ $counter -le $# ]; do
+    case $1 in
+        all|ALL)
+            packages=$all_packages
+            for package in ${packages}
+            do
+                download_package $package
+            done
+            exit 0
+            ;;
+        hdf*|HDF*)
+            download_package HDF5
+            ;;
+        netcdf*|NETCDF*)
+            download_package NETCDF4
+            ;;
+        mpich|MPICH)
+            download_package MPICH
+            ;;
+        grads|GRADS )
+            download_package GRADS
+            ;;
+    esac
+
+    shift
+    let counter=counter+1
+done
+
+exit 0
