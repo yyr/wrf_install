64508b30e15f3295570a058acf757df54ca81923
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-12-01 13:50:48 +0900

    * README.org: readme added

--- build/ALL.sh
+++ build/ALL.sh
@@ -4,6 +4,7 @@
 ./JASPER.sh
 ./HDF5.sh
 ./NETCDF4.sh
+./NCO.sh
 ./MPICH.sh
 ./NCARG.sh # note installs binary for GCC only
 ./WRF.sh
--- build/NCARG.sh
+++ build/NCARG.sh
@@ -1,5 +1,11 @@
 ##!/bin/bash
+. $SCRIPTS_DIR/JASPER.env
+. $SCRIPTS_DIR/ZLIB.env
+. $SCRIPTS_DIR/SZIP.env
+. $SCRIPTS_DIR/HDF5.env
+. $SCRIPTS_DIR/NETCDF4.env
 . $SCRIPTS_DIR/NCARG.env
+
 cd $NCARG_ROOT
 if [ $COMP == "gcc" ]
 then
