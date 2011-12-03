5f531e1a68b28f5c01403778e7294aef89cb6131
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-12-03 10:44:23 +0900

    * build/WRF.sh: close to finish sgi.intel wrf automation

--- build/WRF.sh
+++ build/WRF.sh
@@ -7,8 +7,12 @@ mv ${DIR} $WRF_ROOT
 cd $WRF_ROOT
 
 ./clean -a         # clean first
-./configure # < $SCRIPTS_DIR/build/configure.wrf.${COMP}.select
+./configure  < $SCRIPTS_DIR/build/configure.wrf.${COMP}.select
+
+## auto tweak resulted configure.wrf (common issues followed by system specific)
+$SCRIPTS_DIR/build/configure.wrf.hdf5.sh
+#$SCRIPTS_DIR/build/configure.wrf.sgi.intel.sh # sgi mpi instead of mpich2
 
 # read dummy                            # manual inspection
-#$SCRIPTS_DIR/build/configure.wrf.hdf5.sh
+
 ./compile em_real  2>&1 | tee log.${COMP}.compile
