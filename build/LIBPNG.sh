fa964a73b4de7e773df648a3d406e682857732c9
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-04-15 17:53:13 +0900

    * apps/HDF5.env: dep prefix * build/LIBPNG.sh: configure parameter

--- build/LIBPNG.sh
+++ build/LIBPNG.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+. $appsdir/ZLIB.env
 . $appsdir/LIBPNG.env
 cd $WRF_BASE/src/${DIR}
 
 echo $(pwd)
-./configure --prefix=$LIBPNG_ROOT | tee ${APP}.${COMP}.config
+./configure --prefix=$LIBPNG_ROOT \
+    --with-zlib-prefix=$ZLIB_ROOT | tee ${APP}.${COMP}.config
 
 make clean 2>&1 | tee ${APP}.${COMP}.clean
 make 2>&1 | tee ${APP}.${COMP}.make
