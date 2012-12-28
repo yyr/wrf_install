946e1229f5d897d0230b19fa417dc123b9b5ed9a
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-12-29 07:14:20 +0900

    * .dir-locals.el: fix dir-locals to get env file to sh-mode.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,7 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-(("."
-  (auto-mode-alist . ,(cons auto-mode-alist '("\\.env$" . sh-mode)))))
+((fundamental-mode . ((eval . (when (and (buffer-file-name)
+                                         (file-regular-p (buffer-file-name))
+                                         (string-match-p "\\.env$" (buffer-file-name)))
+                                (sh-mode))))))
