3fddde144852df2a8edb7daad61e3179aadd3034
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-02 06:30:14 +0900

    * build/fix.configure.wrf.sh: use perl instead of awk.

--- build/fix.configure.wrf.sh
+++ build/fix.configure.wrf.sh
@@ -1,5 +1,3 @@
 FILE=configure.wrf
 cp $FILE $FILE.tmp
-awk 'NR==1,/^ LIB_EXTERNAL/' $FILE.tmp > $FILE
-awk '/^ LIB_EXTERNAL/{getline;print $0 " -lhdf5_hl -lhdf5 -lz -lm"}' $FILE.tmp >> $FILE
-awk '/^ LIB_EXTERNAL/,EOF' $FILE.tmp | tail -n+3 >> $FILE
+perl -i -pe "s!(^[ \t]*-L.*)!\$1 -lhdf5_hl -lhdf5 -lz -lm!" configure.wrf
