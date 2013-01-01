4500a62d7ab372d0bc198f8447fbdec5eef89d7f
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-02 06:51:02 +0900

    * SOURCEME: top level variable "dep_root" to common split install   directories installation dependencies.

--- build/fix.configure.wrf.sh
+++ build/fix.configure.wrf.sh
@@ -1,3 +1,3 @@
 FILE=configure.wrf
 cp $FILE $FILE.tmp
-perl -i -pe "s!(^[ \t]*-L.*)!\$1 -lhdf5_hl -lhdf5 -lz -lm!" configure.wrf
+perl -i -pe "s!(^[ \t]*-L.*)!\$1 -L${WRF_BASE}/${dep_root}/lib -lhdf5_hl -lhdf5 -lz -lm!" configure.wrf
