f4aa3e7acce4c87c3c0ba810a849040173495db6
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-02 09:27:22 +0900

    * build/fix.configure.wps.sh: honour new dep_root global variable * build/WPS.sh: cleanup

--- build/WPS.sh
+++ build/WPS.sh
@@ -1,7 +1,7 @@
-################### *IMPORTANT* ######################
-export WRF_EM_CORE=1 # for em core
-# export WRF_NMM_CORE=1 #  for nmm core
-######################################################
+#----------*IMPORTANT*--------------------------------------------------
+export WRF_EM_CORE=1            # for em core
+# export WRF_NMM_CORE=1         #  for nmm core
+#-----------------------------------------------------------------------
 
 # check folder is already present
 if [ ! -d $WRF_BASE/$COMP/${DIR} ]; then
@@ -17,16 +17,24 @@ export WRF_DA_CORE=0
 export JASPERLIB=${JASPER_ROOT}/lib
 export JASPERINC=${JASPER_ROOT}/include
 
-./clean -a # clean first
+export LD_LIBRARY_PATH=${WRF_BASE}/$dep_root/lib:$LD_LIBRARY_PATH
+export LIBINCLUDE="${WRF_BASE}/$dep_root/lib:${WRF_BASE}/$dep_root/include":$LIBINCLUDE
 
+echo NETCDF=$NETCDF
+echo WRFIO_NCD_LARGE_FILE_SUPPORT=$WRFIO_NCD_LARGE_FILE_SUPPORT
+echo WRF_DA_CORE=$WRF_DA_CORE
+echo JASPERLIB=$JASPERLIB
+echo JASPERINC=$JASPERINC
+echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+echo LIBINCLUDE=$LIBINCLUDE
+
+./clean -a
 # ----------- run configure ---------------------------
 # check configure selection file is available
 if [ -f "$SCRIPTS_DIR/${machine}/configure.wps.${COMP}.select" ]; then
-    # selection is available
     ./configure  < $SCRIPTS_DIR/${machine}/configure.wps.${COMP}.select
-
-    else
-
+else
+    ./configure
     echo "
 **IMPORTANT**
 you can automate the selection of configuration option by editing/creating
@@ -34,8 +42,6 @@ you can automate the selection of configuration option by editing/creating
     press ENTER to continue to configure
 "
     read dummy
-    ./configure
-
 fi
 
 # ----------- tweak generated "configure.wps" file -----------------
@@ -45,8 +51,8 @@ $SCRIPTS_DIR/build/fix.configure.wps.sh
 # ----------- tweak generated "configure.wps" file (compiler specific)------------
 # for eg if you want to change configure.wps file put some code in your
 # check fix.configure.wps.intel.sh
+[ -f $SCRIPTS_DIR/${machine}/fix.configure.wps.${COMP}.sh ] &&
 $SCRIPTS_DIR/${machine}/fix.configure.wps.${COMP}.sh
-
 
 # read dummy                      # manual inspection
 ./compile  2>&1 | tee log.${COMP}.compile
--- build/fix.configure.wps.sh
+++ build/fix.configure.wps.sh
@@ -1,13 +1,12 @@
 FILE=configure.wps
 
-# hdf5 should be added
-sed -i 's/-lnetcdff/-lnetcdff  -lhdf5_hl -lhdf5 /g' $FILE
-
+# hdf5
+perl -i -pe "s!-lnetcdff!-lnetcdff -L${WRF_BASE}/${dep_root}/lib -lhdf5_hl -lhdf5 !g" $FILE
 
 # configure.wps doesn't contain compression libs and compression include
 # COMPRESSION_LIBS	= -L$(JASPERLIB)  -ljasper -lpng -lz
 # COMPRESSION_INC		= -I$(JASPERINC) \
 #                        -I$(JASPERINC)/jasper
 
-perl -i -pe "s!(^COMPRESSION_LIBS.*)!\$1 -L\\$\(JASPERLIB\)  -ljasper -lpng -lz!" configure.wps
-perl -i -pe "s!(^COMPRESSION_INC.*)!\$1 -I\\$\(JASPERINC\) -I\\$\(JASPERINC\)/jasper!" configure.wps
+perl -i -pe "s!(^COMPRESSION_LIBS.*)!\$1 -L\\$\(JASPERLIB\)  -ljasper -lpng -lz!" $FILE
+perl -i -pe "s!(^COMPRESSION_INC.*)!\$1 -I\\$\(JASPERINC\) -I\\$\(JASPERINC\)/jasper!" $FILE
