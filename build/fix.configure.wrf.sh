98a1e33b4f6fe24ccc53c48d1103ea8d93b4cb21
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-01-02 09:28:12 +0900

    * build/fix.configure.wrf.sh: use variable defined.

--- build/fix.configure.wrf.sh
+++ build/fix.configure.wrf.sh
@@ -1,3 +1,2 @@
 FILE=configure.wrf
-cp $FILE $FILE.tmp
-perl -i -pe "s!(^[ \t]*-L.*)!\$1 -L${WRF_BASE}/${dep_root}/lib -lhdf5_hl -lhdf5 -lz -lm!" configure.wrf
+perl -i -pe "s!(^[ \t]*-L.*)!\$1 -L${WRF_BASE}/${dep_root}/lib -lhdf5_hl -lhdf5 -lz -lm!" $FILE
