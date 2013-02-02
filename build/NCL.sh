d797e85cbf0de313574e9b33be1c000fc0e6729b
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-02-02 13:24:31 +0900

    * build/NCL.sh: place ld path
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

--- build/NCL.sh
+++ build/NCL.sh
@@ -1,6 +1,12 @@
-cd config
+export LD_LIBRARY_PATH=$WRF_BASE/$dep_root/lib:$WRF_BASE/$COMP/lib
 
+./Configure -v
+
+cd config
 make -f Makefile.ini
 ./ymake -config `pwd`
+
+cd ..
+read dummy
 
 make Everything |& tee make.log
