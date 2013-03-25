ce8dd8221377903fde701fa750dcd66f628462f7
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-03-25 21:29:52 +0900

    * build/G2LIB.sh: new component g2lib
    Signed-off-by: Yakkala Yagnesh Raghava <hi@yagnesh.org>

---
+++ build/G2LIB.sh
@@ -0,0 +1,9 @@
+export CFLAGS="$CFLAGS -DUSE_JPEG2000 -DUSE_PNG"
+
+./configure --prefix=$G2LIB_ROOT \
+    --with-jasper
+    | tee ${APP}.${COMP}.config
+make clean 2>&1 | tee ${APP}.${COMP}.clean
+make 2>&1 | tee ${APP}.${COMP}.make
+make check 2>&1 | tee ${APP}.${COMP}.check
+make install 2>&1 | tee ${APP}.${COMP}.install
