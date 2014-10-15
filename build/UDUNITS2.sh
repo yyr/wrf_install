d8c4d08386de3beb10a8eeea3b227ceed461389d
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-10-15 11:21:24 +0900

    * build/UDUNITS2.sh: use command_runner.

--- build/UDUNITS2.sh
+++ build/UDUNITS2.sh
@@ -1,9 +1,19 @@
-./configure \
+source ${SCRIPTS_DIR}/lib/fun.bash
+
+command_runner \
+    ./configure \
     --prefix=$UDUNITS2_ROOT \
     --exec-prefix=$UDUNITS2_ROOT \
-    --with-pic | tee ${APP}.${COMP}.config
+    --with-pic  ${APP}.${COMP}.config
 
-make clean 2>&1 | tee ${APP}.${COMP}.clean
-make 2>&1 | tee ${APP}.${COMP}.make
-# make check 2>&1 | tee ${APP}.${COMP}.check
-make install 2>&1 | tee ${APP}.${COMP}.install
+command_runner \
+    make clean   ${APP}.${COMP}.clean
+
+command_runner \
+    make   ${APP}.${COMP}.make
+
+# command_runner \
+# make check   ${APP}.${COMP}.check
+
+command_runner \
+    make install   ${APP}.${COMP}.install
