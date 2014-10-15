ef4c0b47c9ead17ecf30d5200491143c9c73e218
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-10-15 13:34:52 +0900

    * apps/NCO.env: increase version number * build/NCO.sh: command_runner version. not successful though.

--- build/NCO.sh
+++ build/NCO.sh
@@ -1,8 +1,14 @@
+source ${SCRIPTS_DIR}/lib/fun.bash
+
 export NETCDF4_ROOT
 export NETCDF_INC
 export NETCDF_LIB
 
-./configure \
+command_runner \
+    ./autogen.sh
+
+command_runner \
+    ./configure \
     --prefix=$NCO_ROOT \
     --includedir=$NETCDF4_ROOT/include \
     --libdir=$NETCDF4_ROOT/lib \
@@ -10,9 +16,13 @@ export NETCDF_LIB
     --enable-netcdf4 \
     --enable-udunits2 \
     --enable-mpi \
-    --disable-dap-netcdf | tee ${APP}.${COMP}.config
+    --disable-dap-netcdf  ${APP}.${COMP}.config
 
-make clean 2>&1 | tee ${APP}.${COMP}.clean
-make 2>&1 | tee ${APP}.${COMP}.make
-# make check 2>&1 | tee ${APP}.${COMP}.check
-make install 2>&1 | tee ${APP}.${COMP}.install
+command_runner \
+    make clean   ${APP}.${COMP}.clean
+command_runner \
+    make   ${APP}.${COMP}.make
+# command_runner \
+# make check   ${APP}.${COMP}.check
+command_runner \
+    make install   ${APP}.${COMP}.install
