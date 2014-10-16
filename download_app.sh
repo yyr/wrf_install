bc38831bf93a1fcfbe68cf0e9d52b0755fa453ce
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-10-16 13:52:34 +0900

    Its bad not to check for certificate.

--- download_app.sh
+++ download_app.sh
@@ -28,8 +28,8 @@ EOF
 function download_package()
 {
     blue_echo "Downloading.... $APP"
-    echo wget -c --no-check-certificate $@ -P $BASE/src -O ${APP}.${EXT}
-    wget -c --no-check-certificate $@ -P $BASE/src -O ${APP}.${EXT}
+    echo wget -c  $@ -P $BASE/src -O ${APP}.${EXT}
+    wget -c  $@ -P $BASE/src -O ${APP}.${EXT}
     if [ $? -ne 0 ]; then
         red_echo "FAILED TO DOWNLOAD.. :( $APP"
         return 64
