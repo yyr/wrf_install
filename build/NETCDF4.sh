560759e017d4428c0e608c26f94ad17731af885f
Phantom139 <rfritzen@phantomdev.net>
2016-09-17 17:44:41 -0500

    All of my changes to update to WRF 3.8, and NETCDF 4.4

--- build/NETCDF4.sh
+++ build/NETCDF4.sh
@@ -1,6 +1,6 @@
 source ${SCRIPTS_DIR}/lib/fun.bash
 
-export CFLAGS="-I${HDF5_ROOT}/include" LDFLAGS="-L${HDF5_ROOT}/lib $CPPFLAGS"
+export CPPFLAGS="-I${HDF5_ROOT}/include -I${ZLIB_ROOT}/include -I${NETCDF4_ROOT}/include" LDFLAGS="-L${HDF5_ROOT}/lib -L${ZLIB_ROOT}/lib -L${NETCDF4_ROOT}/lib" LD_LIBRARY_PATH="${HDF5_ROOT}/lib ${ZLIB_ROOT}/lib ${NETCDF4_ROOT}/lib"
 
 command_runner \
     ./configure \
@@ -12,6 +12,7 @@ command_runner \
     --with-hdf5=$HDF5_ROOT \
     --with-pic \
     --with-libcf \
+    --enable-shared \
     --disable-dap  ${APP}.${COMP}.config
 
 command_runner \
